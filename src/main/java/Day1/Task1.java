751b2e278caefe0efc89145e425ea4d1f6c4ba60
TalanovJS <terror3x@gmail.com>
2022-03-06 16:37:38 +0400

    Tasks

---
+++ src/main/java/Day1/Task1.java
@@ -0,0 +1,21 @@
+package Day1;
+
+/*
+Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя
+цикл while.
+Вывод в консоль должен быть таким:
+JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA
+*/
+public class Task1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i <= 10){
+            System.out.print("JAVA ");
+            i++;
+        }
+
+        for (int j = 0; j <= 10 ; j++) {
+            System.out.print("JAVA ");
+        }
+    }
+}
